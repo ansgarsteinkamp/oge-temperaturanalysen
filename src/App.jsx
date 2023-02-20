0d31b87a9353eb273d97d70689f00bc6ba59c2c6
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-02-20 20:39:24 +0100

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 
-import ScatterPlot from "./UI/ScatterPlot";
+import uniq from "lodash/uniq.js";
+
+import MyScatterPlot from "./UI/ScatterPlot";
 
 const fetchDaten = async (datei, myMap, setState) => {
    const response = await fetch(datei);
@@ -24,7 +26,7 @@ function App() {
 
    // Annahmen zu den txt-Dateien:
    // 1. Excel: Die ID-Spalten sind als Text formatiert, um führende Nullen zu erhalten.
-   // 1. Speichern unter: CSV UTF-8
+   // 1. Speichern unter... CSV UTF-8
    // 2. Dateiendung umbenennen in .txt
    // 3. Überflüssige leere Zeilen am Ende sind entfernt.
 
@@ -35,27 +37,34 @@ function App() {
    useEffect(() => {
       fetchDaten("/stationen.txt", el => ({ id: el[0], name: el[1] }), setStationen);
       fetchDaten("/bezirke.txt", el => ({ bezirk: el[0], id: el[1], prozent: Number(el[2]) }), setBezirke);
-      fetchDaten("/temperaturen.txt", el => ({ id: el[0], datum: el[1], temperatur: Number(el[2]) }), setTemperaturen);
+      fetchDaten("/temperaturen.txt", el => ({ id: el[0], jahr: Number(el[1]), datum: el[2], temperatur: Number(el[3]) }), setTemperaturen);
    }, []);
 
-   const datenSilo = {
-      id: nameDerStation,
-      data: temperaturen.filter(el => el.id === station).map(el => ({ x: el.datum, y: el.temperatur }))
-   };
+   const jahre = uniq(temperaturen.map(el => el.jahr));
 
-   console.log(datenSilo);
+   // Für jedes Jahr ein Array mit den Temperaturen der Station
+   const datenSilo = jahre.map(jahr => ({
+      id: jahr,
+      data: temperaturen.filter(el => el.id === station && el.jahr === jahr).map(el => ({ x: el.datum, y: el.temperatur }))
+   }));
+   // .filter(el => el.id > 2012);
+
+   // const datenSilo = temperaturen.filter(el => el.id === station).map(el => ({ x: el.datum, y: el.temperatur }));
+
+   // Gruppiert nach dem Jahr
+   // let datenSilo = groupBy(temperaturen, el => el.jahr);
+
+   // Array der Struktur [{ id: "2019", data: [{ x: "01-01", y: 7 }, { x: "01-02", y: 5 }, ...] }, ...]
+
+   // console.log(datenSilo);
 
    //  console.log(stationen);
    //  console.log(bezirke);
-   //  console.log(temperaturen);
+   // console.log(datenSilo[0]);
 
    //  console.log(datenSilo[0]);
 
-   return (
-      <div className="mt-10 mx-auto max-w-5xl h-[48rem]">
-         <ScatterPlot />
-      </div>
-   );
+   return <div className="flex justify-center mt-10">{datenSilo.length > 0 && <MyScatterPlot data={datenSilo} />}</div>;
 }
 
 export default App;
