4159c4ad6b59ed39d35894b843399334abad0bb0
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-02-21 15:13:20 +0100

    @tailwindcss/forms + screens + fontSize

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,23 @@
 module.exports = {
    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
    theme: {
-      extend: {}
+      screens: {
+         "3xs": "411px", // Große Smartphones
+         "2xs": "480px", // Monster-Smartphones
+         xs: "540px",
+         sm: "640px",
+         md: "768px",
+         lg: "1024px",
+         xl: "1280px",
+         "2xl": "1536px"
+      },
+      extend: {
+         fontSize: {
+            "2xs": ["0.65rem", { lineHeight: "0.9rem" }],
+            "3xs": ["0.575rem", { lineHeight: "0.8rem" }],
+            "4xs": ["0.5rem", { lineHeight: "0.7rem" }]
+         }
+      }
    },
-   plugins: []
+   plugins: [require("@tailwindcss/forms")]
 };
