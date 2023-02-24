7930820ab1532fa3e39db159e98e4935f3e06c18
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-02-24 16:48:28 +0100

    Refactoring

--- src/UI/Select.jsx
+++ src/UI/Select.jsx
@@ -1,9 +1,13 @@
+import clsx from "clsx";
+
 const Select = ({ label, value, options, onChange, className }) => {
    return (
-      <div className={className}>
+      <div className={clsx("flex items-start", className)}>
          <label>
-            {label}
-            <select value={value} onChange={onChange}>
+            <p className="ml-0.5 mb-1 font-semibold">{label}</p>
+
+            <select className="cursor-pointer" value={value} onChange={onChange}>
+               <option value=""></option>
                {options.map(el => (
                   <option key={el.id} value={el.id}>
                      {el.name}
