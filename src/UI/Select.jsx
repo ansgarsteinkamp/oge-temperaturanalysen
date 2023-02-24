8401a003e42f0056d22f205f96f73e71dabbf0c7
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-02-24 20:15:11 +0100

    Leere Option + label statt name

--- src/UI/Select.jsx
+++ src/UI/Select.jsx
@@ -1,16 +1,16 @@
 import clsx from "clsx";
 
-const Select = ({ label, value, options, onChange, className }) => {
+const Select = ({ label, value, options, onChange, className, leereOption = false }) => {
    return (
       <div className={clsx("flex items-start", className)}>
          <label>
             <p className="ml-0.5 mb-1 font-semibold">{label}</p>
 
             <select className="cursor-pointer" value={value} onChange={onChange}>
-               <option value=""></option>
+               {leereOption && <option value=""></option>}
                {options.map(el => (
                   <option key={el.id} value={el.id}>
-                     {el.name}
+                     {el.label}
                   </option>
                ))}
             </select>
