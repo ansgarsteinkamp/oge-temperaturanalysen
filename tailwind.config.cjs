a70d1f6f02f6d54ef2e76c0f59244eaf68520ae7
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-03-04 10:36:06 +0100

    DANGER-Farben ergänzt

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
    theme: {
@@ -16,6 +18,17 @@ module.exports = {
             "2xs": ["0.65rem", { lineHeight: "0.9rem" }],
             "3xs": ["0.575rem", { lineHeight: "0.8rem" }],
             "4xs": ["0.5rem", { lineHeight: "0.7rem" }]
+         },
+         colors: {
+            gray: colors.stone,
+            "DANGER-800": "#C47373",
+            "DANGER-700": "#CB8585",
+            "DANGER-600": "#D39696",
+            "DANGER-500": "#DAA8A8",
+            "DANGER-400": "#E2B9B9",
+            "DANGER-300": "#E9CBCB",
+            "DANGER-200": "#F0DCDC",
+            "DANGER-100": "#F8EDED"
          }
       }
    },
