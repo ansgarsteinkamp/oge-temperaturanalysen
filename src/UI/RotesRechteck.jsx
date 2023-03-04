45000c0a2c5c6b458a11800c22403e5f350a3534
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-03-04 20:46:30 +0100

    Refactoring

--- src/UI/RotesRechteck.jsx
+++ src/UI/RotesRechteck.jsx
@@ -1,89 +1,69 @@
 import { ResponsiveLine } from "@nivo/line";
+import { addDays, isAfter, format } from "date-fns";
 
-const tag = {
-   1: "01",
-   2: "02",
-   3: "03",
-   4: "04",
-   5: "05",
-   6: "06",
-   7: "07",
-   8: "08",
-   9: "09",
-   10: "10",
-   11: "11",
-   12: "12",
-   13: "13",
-   14: "14",
-   15: "15",
-   16: "16",
-   17: "17",
-   18: "18",
-   19: "19",
-   20: "20",
-   21: "21",
-   22: "22",
-   23: "23",
-   24: "24",
-   25: "25",
-   26: "26",
-   27: "27",
-   28: "28",
-   29: "29",
-   30: "30",
-   31: "31"
+const RotesRechteck = ({ startTag, startMonat, endeTag, endeMonat, minY = -20, maxY = 35, smartphone = false }) => {
+   console.log("startTag", startTag);
+   console.log("startMonat", startMonat);
+   console.log("endeTag", endeTag);
+   console.log("endeMonat", endeMonat);
+
+   const start = new Date(2000, startMonat - 1, startTag);
+   const ende = addDays(new Date(2000, endeMonat - 1, endeTag), 1);
+
+   console.log("start", start);
+   console.log("ende", ende);
+
+   const startString = format(start, "MM-dd");
+   const endeString = format(ende, "MM-dd");
+
+   console.log("startString", startString);
+   console.log("endeString", endeString);
+
+   return (
+      <ResponsiveLine
+         data={[
+            {
+               id: "Rotes Rechteck",
+               data: isAfter(ende, start)
+                  ? [
+                       { x: startString, y: maxY },
+                       { x: endeMonat === 12 && endeTag === 31 ? "13-01" : endeString, y: maxY }
+                    ]
+                  : [
+                       { x: "01-01", y: maxY },
+                       { x: endeString, y: maxY },
+                       { x: endeString, y: minY },
+                       { x: startString, y: minY },
+                       { x: startString, y: maxY },
+                       { x: "13-01", y: maxY }
+                    ]
+            }
+         ]}
+         colors={["#F0DCDC"]}
+         margin={{ top: 10, right: smartphone ? 20 : 50, bottom: 40, left: smartphone ? 50 : 90 }}
+         lineWidth={0}
+         enablePoints={false}
+         enableArea={true}
+         areaBaselineValue={minY}
+         areaOpacity={1}
+         xScale={{
+            type: "time",
+            format: "%m-%d",
+            precision: "day",
+            min: "01-01",
+            max: "13-02"
+         }}
+         yScale={{ type: "linear", min: minY, max: maxY, stacked: false, reverse: false }}
+         axisTop={null}
+         axisRight={null}
+         axisBottom={null}
+         axisLeft={null}
+         isInteractive={false}
+         enableGridX={false}
+         enableGridY={false}
+         enableCrosshair={false}
+      />
+   );
 };
-
-const monat = {
-   1: "01",
-   2: "02",
-   3: "03",
-   4: "04",
-   5: "05",
-   6: "06",
-   7: "07",
-   8: "08",
-   9: "09",
-   10: "10",
-   11: "11",
-   12: "12"
-};
-
-const RotesRechteck = ({ startTag, startMonat, endeTag, endeMonat, minY = -20, maxY = 35, smartphone = false }) => (
-   <ResponsiveLine
-      data={[
-         {
-            id: "Rotes Rechteck",
-            data: [
-               { x: `${monat[startMonat]}-${tag[startTag]}`, y: maxY },
-               { x: endeMonat === 12 && endeTag === 31 ? "13-01" : `${monat[endeMonat]}-${tag[endeTag]}`, y: maxY }
-            ]
-         }
-      ]}
-      colors={["#F0DCDC"]}
-      margin={{ top: 10, right: smartphone ? 20 : 50, bottom: 40, left: smartphone ? 50 : 90 }}
-      lineWidth={0}
-      enablePoints={false}
-      enableArea={true}
-      areaBaselineValue={minY}
-      areaOpacity={1}
-      xScale={{
-         type: "time",
-         format: "%m-%d",
-         precision: "day",
-         min: "01-01",
-         max: "13-02"
-      }}
-      yScale={{ type: "linear", min: minY, max: maxY, stacked: false, reverse: false }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={null}
-      axisLeft={null}
-      isInteractive={false}
-      enableGridX={false}
-      enableGridY={false}
-      enableCrosshair={false}
-   />
-);
 
 export default RotesRechteck;
