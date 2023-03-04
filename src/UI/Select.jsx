cc3e3975a8d8d1de452701b272e784feb05342d5
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-03-04 12:24:55 +0100

    Optionales Icon hinzugefügt

--- src/UI/Select.jsx
+++ src/UI/Select.jsx
@@ -1,19 +1,22 @@
 import clsx from "clsx";
 
-const Select = ({ label, value, options, onChange, className, leereOption = false, minWidth = null }) => {
+const Select = ({ label, value, options, onChange, className, leereOption = false, minWidth = null, icon = null }) => {
    return (
       <div className={clsx("flex items-start", className)}>
          <label>
             <p className="ml-1 mb-1 font-semibold">{label}</p>
 
-            <select className={clsx("cursor-pointer", minWidth)} value={value} onChange={onChange}>
-               {leereOption && <option value=""></option>}
-               {options.map(el => (
-                  <option key={el.id} value={el.id}>
-                     {el.label}
-                  </option>
-               ))}
-            </select>
+            <div className="flex items-center space-x-1">
+               <select className={clsx("cursor-pointer", minWidth)} value={value} onChange={onChange}>
+                  {leereOption && <option value=""></option>}
+                  {options.map(el => (
+                     <option key={el.id} value={el.id}>
+                        {el.label}
+                     </option>
+                  ))}
+               </select>
+               {icon}
+            </div>
          </label>
       </div>
    );
