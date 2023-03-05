96b291191072237abded5ea8b42f45ed8b2b370e
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-03-05 18:10:46 +0100

    Return nur der Verteilungsfunktion, nicht der Daten für nivo

--- src/utils/temperaturenZuVerteilungsfunktion.js
+++ src/utils/temperaturenZuVerteilungsfunktion.js
@@ -1,11 +1,9 @@
 import { parse, getYear, setYear, isAfter, isBefore } from "date-fns";
+
 import zip from "lodash/zip.js";
 import sortBy from "lodash/sortBy.js";
 
-const temperaturReferenz = [
-   -20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-   19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
-];
+import temperaturenReferenz from "./temperaturenReferenz.js";
 
 // temperaturen: [ 0.2, -3.1, ... ]
 // xAchse: [ "2003-01-01", "2003-01-02", ... ]
@@ -29,12 +27,10 @@ const temperaturenZuVerteilungsfunktion = (temperaturen, xAchse, startJahr, star
 
    const gesamtZahl = temperaturenSortiert.length;
 
-   const verteilungsfunktion = temperaturReferenz.map(el => ({
+   return temperaturenReferenz.map(el => ({
       x: el,
       y: gesamtZahl === 0 ? 0 : temperaturenSortiert.filter(t => t < el).length / gesamtZahl
    }));
-
-   return [{ id: "Verteilungsfunktion", data: verteilungsfunktion }];
 };
 
 export default temperaturenZuVerteilungsfunktion;
