bce11050a0ab5a5d010fcbbd4e374067e06c558c
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-03-09 11:12:15 +0100

    Interactive + Tooltip

--- src/UI/ScatterPlot.jsx
+++ src/UI/ScatterPlot.jsx
@@ -1,6 +1,8 @@
 import { ResponsiveScatterPlotCanvas } from "@nivo/scatterplot";
 
-const ScatterPlot = ({ data, minY = -20, maxY = 35, smartphone = false }) => (
+import { parseJSON, intlFormat } from "date-fns";
+
+const ScatterPlot = ({ data, minY = -20, maxY = 35, temperaturArt, smartphone = false }) => (
    <ResponsiveScatterPlotCanvas
       nodeSize={smartphone ? 2.5 : 4.5}
       theme={{
@@ -36,7 +38,23 @@ const ScatterPlot = ({ data, minY = -20, maxY = 35, smartphone = false }) => (
          tickSize: 0,
          tickPadding: smartphone ? 7 : 10
       }}
-      isInteractive={false} // Macht hier keinen Sinn, da useMesh={false} nicht unterstützt wird.
+      isInteractive={true}
+      yFormat=" >-.2~f"
+      tooltip={el => (
+         <div
+            className="text-white bg-DANGER-800 py-2 px-4 rounded"
+            style={{
+               fontSize: "90%"
+            }}
+         >
+            <p>
+               {intlFormat(parseJSON(el.node.xValue), { month: "long", day: "2-digit" }, { locale: "de-DE" })} {el.node.serieId}
+            </p>
+            <p>
+               {temperaturArt}: {el.node.formattedY.replace(".", ",")} °C
+            </p>
+         </div>
+      )}
       // useMesh={false} // Wird bei Canvas nicht unterstützt.
       // animate={false} // Wird bei Canvas nicht unterstützt.
    />
