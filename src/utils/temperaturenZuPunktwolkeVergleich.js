66aad21290b2a9c978b1939f8c18b62e0a9aa082
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-03-09 12:19:08 +0100

    Refactoring: Gruppiert nach jedem einzelnen Tag

--- src/utils/temperaturenZuPunktwolkeVergleich.js
+++ src/utils/temperaturenZuPunktwolkeVergleich.js
@@ -8,30 +8,28 @@ import map from "lodash/map.js";
 // temperaturenVergleich: [ 0.2, -3.1, ... ]
 // xAchse: [ "2003-01-01", "2003-01-02", ... ]
 
-// => [ { id: 2003, data: [ {x: Temperatur, y: Temperatur}, ... ] }, ...]
+// => [ { id: "2003-01-01", data: [ {x: Temperatur, y: Temperatur} ] }, ...]
 
 const temperaturenZuPunktwolkeVergleich = (temperaturen, temperaturenVergleich, xAchse, startJahr, startTag, startMonat, endeTag, endeMonat) => {
    const start = new Date(2000, startMonat - 1, startTag);
    const ende = new Date(2000, endeMonat - 1, endeTag);
 
    const punktwolke = zip(
+      xAchse,
       xAchse.map(el => parse(el, "yyyy-MM-dd", new Date())),
       temperaturen,
       temperaturenVergleich
    )
-      .filter(el => getYear(el[0]) >= startJahr)
-      .map(el => ({ datum: el[0], datum2000: setYear(el[0], 2000), temperatur: el[1], temperaturVergleich: el[2] }))
+      .filter(el => getYear(el[1]) >= startJahr)
+      .map(el => ({ datum: el[0], dateObjekt2000: setYear(el[1], 2000), temperatur: el[2], temperaturVergleich: el[3] }))
       .filter(el =>
          !isBefore(ende, start)
-            ? !isBefore(el.datum2000, start) && !isAfter(el.datum2000, ende)
-            : !isAfter(el.datum2000, ende) || !isBefore(el.datum2000, start)
+            ? !isBefore(el.dateObjekt2000, start) && !isAfter(el.dateObjekt2000, ende)
+            : !isAfter(el.dateObjekt2000, ende) || !isBefore(el.dateObjekt2000, start)
       )
-      .map(el => ({ jahr: getYear(el.datum), x: el.temperatur, y: el.temperaturVergleich }));
+      .map(el => ({ id: el.datum, data: [{ x: el.temperatur, y: el.temperaturVergleich }] }));
 
-   return map(
-      groupBy(punktwolke, el => el.jahr),
-      (xyzWerte, jahr) => ({ id: jahr, data: xyzWerte.map(xyz => ({ x: xyz.x, y: xyz.y })) })
-   );
+   return punktwolke;
 };
 
 export default temperaturenZuPunktwolkeVergleich;
