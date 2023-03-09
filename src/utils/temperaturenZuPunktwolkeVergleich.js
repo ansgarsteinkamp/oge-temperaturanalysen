518336b7c457f2033de8ba2b5dbf9f8876fe0e6c
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-03-09 11:52:14 +0100

    Refactoring: Gruppiert nach Jahr

--- src/utils/temperaturenZuPunktwolkeVergleich.js
+++ src/utils/temperaturenZuPunktwolkeVergleich.js
@@ -2,12 +2,13 @@ import { parse, getYear, setYear, isAfter, isBefore } from "date-fns";
 
 import zip from "lodash/zip.js";
 import groupBy from "lodash/groupBy.js";
+import map from "lodash/map.js";
 
 // temperaturen: [ 0.2, -3.1, ... ]
 // temperaturenVergleich: [ 0.2, -3.1, ... ]
 // xAchse: [ "2003-01-01", "2003-01-02", ... ]
 
-// => [ { id: "Temperaturenvergleich", data: [ {x: Temperatur, y: Temperatur}, ... ] }, ...]
+// => [ { id: 2003, data: [ {x: Temperatur, y: Temperatur}, ... ] }, ...]
 
 const temperaturenZuPunktwolkeVergleich = (temperaturen, temperaturenVergleich, xAchse, startJahr, startTag, startMonat, endeTag, endeMonat) => {
    const start = new Date(2000, startMonat - 1, startTag);
@@ -19,11 +20,18 @@ const temperaturenZuPunktwolkeVergleich = (temperaturen, temperaturenVergleich, 
       temperaturenVergleich
    )
       .filter(el => getYear(el[0]) >= startJahr)
-      .map(el => ({ datum: setYear(el[0], 2000), temperatur: el[1], temperaturVergleich: el[2] }))
-      .filter(el => (!isBefore(ende, start) ? !isBefore(el.datum, start) && !isAfter(el.datum, ende) : !isAfter(el.datum, ende) || !isBefore(el.datum, start)))
-      .map(el => ({ x: el.temperatur, y: el.temperaturVergleich }));
+      .map(el => ({ datum: el[0], datum2000: setYear(el[0], 2000), temperatur: el[1], temperaturVergleich: el[2] }))
+      .filter(el =>
+         !isBefore(ende, start)
+            ? !isBefore(el.datum2000, start) && !isAfter(el.datum2000, ende)
+            : !isAfter(el.datum2000, ende) || !isBefore(el.datum2000, start)
+      )
+      .map(el => ({ jahr: getYear(el.datum), x: el.temperatur, y: el.temperaturVergleich }));
 
-   return [{ id: "Temperaturenvergleich", data: punktwolke }];
+   return map(
+      groupBy(punktwolke, el => el.jahr),
+      (xyzWerte, jahr) => ({ id: jahr, data: xyzWerte.map(xyz => ({ x: xyz.x, y: xyz.y })) })
+   );
 };
 
 export default temperaturenZuPunktwolkeVergleich;
